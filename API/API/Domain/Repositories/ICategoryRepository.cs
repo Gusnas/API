509f010c65db7970eb854acf64a061aa9fd6957c
Gustavo Nascimento <gustavo.nascimento@fitbank.com.br>
2021-11-18 17:10:48 -0300

    Criacao da base da API do curso de backend .net

---
+++ API/API/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Domain.Models;
+
+namespace API.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
